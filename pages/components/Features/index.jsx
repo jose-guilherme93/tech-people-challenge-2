8f6771c56468ccd998ab7ed4776e84904a7be3cc
jose-guilherme93 <jose-guilherme93@hotmail.com>
2022-10-21 17:44:32 -0300

    fix-- prerender content in features and news

--- pages/components/Features/index.jsx
+++ pages/components/Features/index.jsx
@@ -1,40 +1,40 @@
 /* eslint-disable @next/next/no-img-element */
 import styles from './style.module.css'
 
- export default function Features(props) {
-    
+import {commitments} from '../../index'
+ export default function Features() {
     return (
         <>
             <div className={styles.wrapper}>
                 <section className={styles.features}>
-                    <img className={styles.starPhoto} src={props.icon.starPhoto} alt={props.alt[0]} />
-                    <h4>{props.subtitle[0]}</h4>
-                    <p>{props.paragraph[0]}</p>
+                    <img className={styles.starPhoto} src={commitments.icon.starPhoto} alt={commitments.alt[0]} />
+                    <h4>{commitments.subtitle[0]}</h4>
+                    <p>{commitments.paragraph[0]}</p>
                 </section>
                 <section className={styles.features}>
-                    <img className={styles.hatPhoto} src={props.icon.hatPhoto} alt={props.alt[1]} />
-                    <h4>{props.subtitle[1]}</h4>
-                    <p>{props.paragraph[1]}</p>
+                    <img className={styles.hatPhoto} src={commitments.icon.hatPhoto} alt={commitments.alt[1]} />
+                    <h4>{commitments.subtitle[1]}</h4>
+                    <p>{commitments.paragraph[1]}</p>
                 </section>
                 <section className={styles.features}>
-                    <img className={styles.feedbackPhoto} src={props.icon.feedbackPhoto} alt={props.alt[2]}/>
-                    <h4>{props.subtitle[2]}</h4>
-                    <p>{props.paragraph[2]}</p>
+                    <img className={styles.feedbackPhoto} src={commitments.icon.feedbackPhoto} alt={commitments.alt[2]}/>
+                    <h4>{commitments.subtitle[2]}</h4>
+                    <p>{commitments.paragraph[2]}</p>
                 </section>
                 <section className={styles.features}>
-                    <img className={styles.weatherCockPhoto} src={props.icon.weatherCockPhoto} alt={props.alt[3]}/>
-                    <h4>{props.subtitle[3]}</h4>
-                    <p>{props.paragraph[3]}</p>
+                    <img className={styles.weatherCockPhoto} src={commitments.icon.weatherCockPhoto} alt={commitments.alt[3]}/>
+                    <h4>{commitments.subtitle[3]}</h4>
+                    <p>{commitments.paragraph[3]}</p>
                 </section>
                 <section className={styles.features}>
-                    <img className={styles.timerPhoto} src={props.icon.timerPhoto} alt={props.alt[4]}/>
-                    <h4>{props.subtitle[4]}</h4>
-                    <p>{props.paragraph[4]}</p>
+                    <img className={styles.timerPhoto} src={commitments.icon.timerPhoto} alt={commitments.alt[4]}/>
+                    <h4>{commitments.subtitle[4]}</h4>
+                    <p>{commitments.paragraph[4]}</p>
                 </section>  
                 <section className={styles.features}>
-                    <img className={styles.bagPhoto} src= {props.icon.bagPhoto}alt={props.alt[5]}/>
-                    <h4>{props.subtitle[5]}</h4>
-                    <p>{props.paragraph[5]}</p>
+                    <img className={styles.bagPhoto} src= {commitments.icon.bagPhoto}alt={commitments.alt[5]}/>
+                    <h4>{commitments.subtitle[5]}</h4>
+                    <p>{commitments.paragraph[5]}</p>
                 </section>
             </div>
         </>
--- pages/components/News/index.jsx
+++ pages/components/News/index.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 import styles from './style.module.css'
 
-export default function News(props) {
+import {infoNews} from '../../index'
+export default function News() {
     
     return (
         <>  
@@ -10,22 +11,22 @@ export default function News(props) {
                 <p>Listamos abaixo algumas notícias de tecnologia que nos motivam a estar ativos diariamente neste nosso projeto.</p>
                 <section className={styles.gridContainer}>
                     <div className={styles.date}>
-                        <img src={props.image[0]} alt="wer" />
+                        <img src={infoNews.image[0]} alt="wer" />
                         <img className={styles.calendarImage} src="/img-news/calendar.png" alt="um pequeno ícone de calendário com uma cor sólida escura" />
                         <span>17 abr 2020</span>
                         <a href="https://www.tabnews.com.br/" rel='noopener noreferrer' target="_blank"><p>Participe da nossa Live sobre React e testes TDD</p></a>
                     </div>
                     <div className={styles.date}>                        
-                        <img src={props.image[1]} alt="calendário" />
+                        <img src={infoNews.image[1]} alt="calendário" />
                         <img className={styles.calendarImage} src="/img-news/calendar.png" alt="" />
                         <span>17 abr 2020</span>
-                        <a href="https://www.tabnews.com.br/" rel='noopener noreferrer' target="_blank"><p>{props.paragraph}</p></a>
+                        <a href="https://www.tabnews.com.br/" rel='noopener noreferrer' target="_blank"><p>{infoNews.paragraph}</p></a>
                     </div>
                     <div className={styles.date}>         
-                        <img src={props.image[2]} alt="calendário" />
+                        <img src={infoNews.image[2]} alt="calendário" />
                         <img className={styles.calendarImage} src="/img-news/calendar.png" alt="calendário" /> 
-                        <span>{props.date}</span>
-                        <p><a href="https://www.tabnews.com.br/" rel='noopener noreferrer' target="_blank">{props.paragraph}</a></p>
+                        <span>{infoNews.date}</span>
+                        <p><a href="https://www.tabnews.com.br/" rel='noopener noreferrer' target="_blank">{infoNews.paragraph}</a></p>
                     </div>
 
                 </section>
