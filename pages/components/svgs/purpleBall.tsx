a34725aaa9c77aa480a751776a42a3c2a290c415
jose-guilherme93 <jose-guilherme93@hotmail.com>
2023-06-23 18:08:15 -0300

    refactor: :recycle: created svg component folder

---
+++ pages/components/svgs/purpleBall.tsx
@@ -0,0 +1,34 @@
+
+
+export default function PurpleBall() {
+
+    return (
+        <svg 
+                  
+                  width="579" 
+                  height="581" 
+                  viewBox="0 0 579 581" 
+                  fill="none" 
+                  xmlns="http://www.w3.org/2000/svg">
+                  <ellipse 
+                    opacity="0.702497" 
+                    cx="515" 
+                    cy="290.5" 
+                    rx="515" 
+                    ry="290.5" 
+                    fill="url(#paint0_linear_0_10)"/>
+                  <defs>
+                    <linearGradient 
+                      id="paint0_linear_0_10" 
+                      x1="243.675" 
+                      y1="-21.768" 
+                      x2="213.797" 
+                      y2="285.353" 
+                      gradientUnits="userSpaceOnUse">
+                      <stop stopColor="#8471FF"/>
+                      <stop offset="1" stopColor="#717CFF"/>
+                    </linearGradient>
+                  </defs>
+                </svg>
+    )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -10,6 +10,7 @@ import Agenda   from './components/Agenda'
 import Form     from './components/Form'
 import Footer   from './components/Footer'
 
+import PurpleBall from './components/svgs/purpleBall'
 import styles   from '../styles/Home.module.css'
 
 export default function Home() {
@@ -63,33 +64,7 @@ export default function Home() {
 
             <section className={styles.asideImages}>
               <div className={styles.wrapperImage}>
-                <svg 
-                  className={styles.purpleBall} 
-                  width="579" 
-                  height="581" 
-                  viewBox="0 0 579 581" 
-                  fill="none" 
-                  xmlns="http://www.w3.org/2000/svg">
-                  <ellipse 
-                    opacity="0.702497" 
-                    cx="515" 
-                    cy="290.5" 
-                    rx="515" 
-                    ry="290.5" 
-                    fill="url(#paint0_linear_0_10)"/>
-                  <defs>
-                    <linearGradient 
-                      id="paint0_linear_0_10" 
-                      x1="243.675" 
-                      y1="-21.768" 
-                      x2="213.797" 
-                      y2="285.353" 
-                      gradientUnits="userSpaceOnUse">
-                      <stop stopColor="#8471FF"/>
-                      <stop offset="1" stopColor="#717CFF"/>
-                    </linearGradient>
-                  </defs>
-                </svg>
+                <PurpleBall className={styles.PurpleBall} />
               </div>
 
               
